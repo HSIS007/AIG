2ae068af3559bdd2c50110f2232e1494ce52155c
Pravesh Janartha <er.pravesh786@gmail.com>
2016-01-11 10:22:42 +0530

    Update AIG.py

--- AIG.py
+++ AIG.py
@@ -7,11 +7,11 @@ print   " ########        #       ######### "
 print  " #        #       #               # "
 print " #          #  #########           # V1.0 "   
 print " Created by HSIS007 AKA(SZZER)"
-print " Active Information Gathering tool for pentester, so far this tools only connects to the host and grabs the banners"
+print " Active Information Gathering tool for pentester, so far this script connects to the host and informs the pentester about the banner on the selected port."
 import optparse
 import socket
 from socket import*
-def connScan(Host,Port): #Takes two arguments host and port
+def connection(Host,Port): #Takes two arguments host and port
 	try:
 		fetchSkt = socket(AF_INET,SOCK_STREAM) #this creates object with IPv4 and tcp connection
 		
@@ -38,9 +38,9 @@ def portScan(Host,Ports):   #This function takes the hostname and targetport as 
 	setdefaulttimeout(1)
 	for Port in Ports:
 		print 'Scanning Port ' + Port
-		connScan(Host,int(Port))
+		connection(Host,int(Port))
 def main():
-	parser = optparse.OptionParser('usage Fetcher.py -H <IP Address host> -p <target port>') #Creates an instance of an option parser
+	parser = optparse.OptionParser('usage AIG.py -H <IP Address host> -p <target port>') #Creates an instance of an option parser
 	parser.add_option('-H',dest='Host',type='string',help='specify target host') #Specifies the individual command line options for script
 	parser.add_option('-p',dest='Port',type='int',help='enter ports seprated by , to scan for')
 	(options,args) = parser.parse_args()
